c4adaf0ea33697560b320bdf27e4e15c18e53e84
yawei <d>
2019-05-16 22:33:43 +0800

    外观设计模式

---
+++ src/main/java/FacadePattern/FacadePatternDemo.java
@@ -0,0 +1,14 @@
+package FacadePattern;
+
+//外观模式
+//隐藏了系统的复杂性
+//提供一个类 客户端可以简化调用 系统复杂实现的方法
+public class FacadePatternDemo {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
